edcb768cc48351dc2d715af5c80f600b5a134e2d
[Leontec]Long Vu <vlong@leontec.co.jp>
2023-11-27 10:44:34 +0700

    fix marginBottom

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
   return <div>
-    <h2 style={{ marginBottom: 0 }}>404</h2>
+    <h2 style={{ marginBottom: 15 }}>404</h2>
     <h4 style={{ marginTop: 0 }}>Page Not Found!</h4>
   </div>
 }
